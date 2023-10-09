ba46aa97a2b96f29121bc2aa379f231961fe58cc
Binh Duc Tran <binhtran432k@gmail.com>
2023-10-09 11:14:34 +0700

    feat: make js lab output readable

--- PRF192x_2.1-A_VN/lab_first.js
+++ PRF192x_2.1-A_VN/lab_first.js
@@ -4,10 +4,10 @@ const population = 39908501;
 const isIsland = false;
 let language = "Vietnamese";
 
-console.log(isIsland);
-console.log(population);
-console.log(country);
-console.log(language);
+console.log(`isIsland: ${isIsland}`);
+console.log(`population: ${population}`);
+console.log(`country: ${country}`);
+console.log(`language: ${language}`);
 
 language = "English";
-console.log(language);
+console.log(`language: ${language}`);
