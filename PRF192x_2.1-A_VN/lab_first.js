45134b09978cf83ee6858320d4fa58f56a73797a
Binh Duc Tran <binhtran432k@gmail.com>
2023-10-09 11:10:32 +0700

    feat: add lab 2.2.2

--- PRF192x_2.1-A_VN/lab_first.js
+++ PRF192x_2.1-A_VN/lab_first.js
@@ -2,9 +2,12 @@ const country = "Vietnam";
 const continent = "Asia";
 const population = 39908501;
 const isIsland = false;
-let language;
+let language = "Vietnamese";
 
 console.log(isIsland);
 console.log(population);
 console.log(country);
+console.log(language);
+
+language = "English";
 console.log(language);
