747221e7b663e547b93d4f083c669fd54fe6bc61
Binh Duc Tran <binhtran432k@gmail.com>
2023-10-10 17:03:15 +0700

    feat: add lab 2.3.4

--- PRF192x_2.1-A_VN/lab_3.js
+++ PRF192x_2.1-A_VN/lab_3.js
@@ -46,4 +46,29 @@ function findCountry() {
   }
 }
 
-findCountry();
+function checkLanguage() {
+  const language = prompt("language? ");
+  switch (language) {
+    case "Chinese":
+    case "Mandarin":
+      console.log("MOST number of native speakers!");
+      break;
+    case "Spanish":
+      console.log("2nd place in number of native speakers");
+      break;
+    case "English":
+      console.log("3rd place");
+      break;
+    case "Hindi":
+      console.log("Number 4");
+      break;
+    case "Arabic":
+      console.log("5th most spoken language");
+      break;
+    default:
+      console.log("Great language too :D");
+      break;
+  }
+}
+
+checkLanguage();
