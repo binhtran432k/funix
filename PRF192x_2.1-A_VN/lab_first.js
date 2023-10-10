730a0d194f47d59dac4f1e78c918e81a53ee7f70
Binh Duc Tran <binhtran432k@gmail.com>
2023-10-10 11:48:33 +0700

    feat: add lab 2.2.3

--- PRF192x_2.1-A_VN/lab_first.js
+++ PRF192x_2.1-A_VN/lab_first.js
@@ -1,8 +1,10 @@
 const country = "Vietnam";
 const continent = "Asia";
-const population = 39908501;
+let population = 39908501;
 const isIsland = false;
 let language = "Vietnamese";
+const finlandPopulation = 6000000;
+const averagePopulation = 33000000;
 
 console.log(`isIsland: ${isIsland}`);
 console.log(`population: ${population}`);
@@ -11,3 +13,23 @@ console.log(`language: ${language}`);
 
 language = "English";
 console.log(`language: ${language}`);
+
+console.log(`population / 2: ${population / 2}`);
+console.log("-> Increase population by 1");
+population += 1;
+console.log(`population: ${population}`);
+console.log(
+  `is population bigger than Finland's population: ${
+    population > finlandPopulation
+  }`,
+);
+console.log(
+  `is population lesser than average population: ${
+    population < averagePopulation
+  }`,
+);
+console.log(
+  `${country} and its ${(population / 1000000).toFixed(
+    2,
+  )} million people speak ${language}`,
+);
