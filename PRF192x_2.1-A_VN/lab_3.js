2742c3645403fe507424a9e111d7ea33da4e9efd
Binh Duc Tran <binhtran432k@gmail.com>
2023-10-10 15:27:25 +0700

    feat: add lab 2.3.2

--- PRF192x_2.1-A_VN/lab_3.js
+++ PRF192x_2.1-A_VN/lab_3.js
@@ -1,3 +1,6 @@
+// Setup prompt
+const prompt = require("prompt-sync")();
+
 function fu3_1() {
   console.log("9" - "5"); // 4
   console.log("19" - "13" + "17"); // 617
@@ -6,4 +9,17 @@ function fu3_1() {
   console.log(5 + 6 + "4" + 9 - 4 - 2); // 1143
 }
 
-fu3_1();
+function fu3_2() {
+  const numNeighbours = prompt(
+    "How many neighbor countries does your country have? ",
+  );
+  if (Number(numNeighbours) === 1) {
+    console.log("Only 1 border!");
+  } else if (numNeighbours > 1) {
+    console.log("More than 1 border!");
+  } else {
+    console.log("No borders");
+  }
+}
+
+fu3_2();
