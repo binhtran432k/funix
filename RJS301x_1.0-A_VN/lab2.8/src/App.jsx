2be68e9cbdf72a4a8c7aacc616de0bfc46fb4b4b
Binh Duc Tran <binhtran432k@gmail.com>
2023-11-22 20:48:11 +0700

    feat(reactjs): add lab 2.8

---
+++ RJS301x_1.0-A_VN/lab2.8/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import { Expenses } from "./components/Expenses";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <h1>Let&apos;s get started</h1>
+      <Expenses />
+    </div>
+  );
+};
---
+++ RJS301x_1.0-A_VN/lab2.8/src/components/ExpenseItem.jsx
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types";
+import { ExpenseDate } from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+/**
+ * @typedef ExpenseProp
+ * @property {string} id
+ * @property {string} title
+ * @property {number} amount
+ * @property {Date} date
+ */
+
+/**
+ * @type {React.FC<ExpenseProp>}
+ */
+const ExpenseItem = (props) => {
+  return (
+    <div className="ExpenseItem">
+      <div className="expense-title-container">
+        <ExpenseDate date={props.date} />
+        <div className="expense-title">{props.title}</div>
+      </div>
+      <div className="expense-amount">${props.amount}</div>
+    </div>
+  );
+};
+
+ExpenseItem.propTypes = {
+  id: PropTypes.string,
+  title: PropTypes.string,
+  amount: PropTypes.number,
+  date: PropTypes.objectOf(Date),
+};
+
+export { ExpenseItem };
