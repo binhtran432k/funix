a01abb9e7fc312b1e54f58ea9a5222b946e89734
Binh Duc Tran <binhtran432k@gmail.com>
2023-11-24 19:29:51 +0700

    fix(reactjs/asm1): fix lack of key in Type

--- RJS301x_1.0-A_VN/asm1/src/components/type/Type.jsx
+++ RJS301x_1.0-A_VN/asm1/src/components/type/Type.jsx
@@ -8,7 +8,7 @@ const Type = () => {
         <h2 className="type__title">Browse by property type</h2>
         <div className="type__list">
           {types.map((type) => (
-            <div className="type__item">
+            <div className="type__item" key={type.name}>
               <img src={type.image} alt={type.name} className="type__image" />
               <div className="type__name">{type.name}</div>
               <div className="type__subtext">{type.count} hotels</div>
